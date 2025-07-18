d4eb5c14ffb85026015d545f193255a3fcb7d232
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-07-18 23:37:09 +0800

    add used files

---
+++ test-js/script.js
@@ -0,0 +1,45 @@
+
+
+
+// ----------------------------------------------------------------------------------
+// simple user / password checker
+
+// let userName = prompt("Please Enter your name:");
+
+// if (userName === "Admin" || userName === "admin") {
+//     adminPassword = prompt("Enter Password:");
+//     if (adminPassword == "TheMaster"){
+//         alert("Welcome Master");
+//     }else if (adminPassword === null || adminPassword === ""){
+//         alert("Cancelled");
+//     }else {
+//         alert("Wrong Password");
+//     }
+// }else if (userName === null){
+//     alert("Cancelled");
+// }else {
+//     alert("I don't know you!");
+// }
+// ----------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------
+// to check if input text is in the array |~ un-comment the <input> & <p> tag before running ~|
+
+// let a = ['apple', 'orange', 'watermelon'];
+// let input = document.getElementById("text");
+// let output = document.getElementById("test");
+
+// input.addEventListener("input", function(){
+//     for(let x of a){
+//         if (x === input.value){
+//             output.innerText = x;
+//             return;
+//         } else if (input.value === ""){
+//             output.innerText = "";
+//         }else {
+//             output.innerText = "Not found"; 
+//         }
+//     }
+    
+// })
+// ----------------------------------------------------------------------------------
