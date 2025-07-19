47c1af5b6610986289bff68acf66c5abc84cf182
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-07-19 18:08:44 +0800

    FizzBuzz Solution

--- test-js/script.js
+++ test-js/script.js
@@ -1,5 +1,46 @@
+let userNum = parseInt(prompt("Enter a number for FizzBuzz: "));
+let answers = [];
+
+for (let x = 0; x <= userNum; x++){
+
+    if (x % 3 === 0 && x % 5 === 0 ){
+        answers[x] = " FizzBuzz ";
+    }
+    else if (x % 5 === 0){
+        answers[x] = " Buzz ";
+    }
+    else if (x % 3 === 0 ){
+        answers[x] = " Fizz ";
+    }
+    else {
+        answers[x] = x;
+    }
+
+    if (x == userNum){
+        console.log(answers);
+    }
+}
 
 
+// function add7(numForAdd){
+//     return numForAdd + 7;
+// }
+
+// function multiply(num1ForMultiply, num2ForMultiply){
+//     num1ForMultiply = prompt("Enter first Number:");
+//     num2ForMultiply = prompt("Enter second Number:");
+//     return alert(num1ForMultiply * num2ForMultiply);
+// }
+
+// function capitalize(){
+//     let newWord = prompt("Enter a sting:");
+//     return alert(newWord.slice(0,1).toUpperCase() + newWord.slice(1));
+// }               //return first letter only --     //return all characters starting at index[1]
+
+// function lastletter(){
+//     let newWord = prompt("Enter a sting:");
+//     document.getElementById("text").innerHTML = newWord.slice(-1);
+// }
 
 // ----------------------------------------------------------------------------------
 // simple user / password checker
