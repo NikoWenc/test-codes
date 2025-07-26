6f4ac1f7d4b0d8fa83f23b58aa0222b79b507d01
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-07-27 00:30:37 +0800

    sorting an array

--- dom-manipulation/test.js
+++ dom-manipulation/test.js
@@ -1,13 +1,5 @@
-let arr = [5, 3, 8, 1];
+let arr = [5, 2, 1, -10, 8, 11];
 
-function filterRangeInPlace(array, a, b){
-    return array.filter((item, index) => {
-        if (item < a || item > b){
-            return array.splice(index,1);
-        }
-    })
-}
+arr.sort((a, b) => b - a);
 
-filterRangeInPlace(arr, 1, 4);
-
-alert(arr)
+alert( arr );
