165fb95866eea5fe2e55770c0ee16e98dea7fc98
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-07-28 23:25:00 +0800

    some test codes for manipulating the DOM

--- dom-manipulation/test.js
+++ dom-manipulation/test.js
@@ -1,11 +1,41 @@
+const container = document.querySelector("#container");
+const contentDiv = document.createElement("div");
 
-let userInput = prompt("Calculator");
+contentDiv.classList.toggle("content");
+contentDiv.textContent = "This is the glorious text-content!";
+container.appendChild(contentDiv);
 
-function calculator(input){
-    return alert(eval(input));
-    // eval is not safe to use
-    // install mathjs for safety measures
-    // can be "return alert(math.evaluate(input))"
-}
+// a <p> with red text that says “Hey I’m red!”
+const redPara = document.createElement("p");
+redPara.setAttribute("style", "color: red");
+redPara.textContent = "Hi I'm Red!";
+container.appendChild(redPara);
 
-calculator(userInput);
+// an <h3> with blue text that says “I’m a blue h3!”
+const newH3 = document.createElement("h3");
+newH3.setAttribute("style", "color: blue");
+newH3.textContent = `I\’m a blue h3!`;
+container.appendChild(newH3);
+
+// a <div> with a black border and pink background color with the following elements inside of it:
+// another <h1> that says “I’m in a div”
+const newH1 = document.createElement("h1");
+const blackBorderDiv = document.createElement("div");
+blackBorderDiv.setAttribute("style", "border: 1px solid black; background-color: pink");
+newH1.textContent = `I\’m in a div`;
+blackBorderDiv.appendChild(newH1);
+
+//a <p> that says “ME TOO!”
+const meTooPara = document.createElement("p");
+meTooPara.textContent = "ME TOO!";
+blackBorderDiv.appendChild(meTooPara);
+// append to container div
+container.appendChild(blackBorderDiv);
+
+const allBtn = document.querySelectorAll(".button");
+
+allBtn.forEach((item) => {
+    item.addEventListener("click", () => {
+        alert("Hi");
+    })
+})
