93b5ad12140ead6028bc06154450ffa6e5b045a6
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-08-21 15:44:51 +0800

    test object constructor again with throw Error to make sure New is used

--- test-constructor/script.js
+++ test-constructor/script.js
@@ -1,30 +1,57 @@
-function Book (tittle, author, pages) {
-    if (!new.target){
-        // log this error if 'new' operator is not called
-        throw Error("You must use the 'new' operator to call the constructor")
+// function Book (tittle, author, pages) {
+//     if (!new.target){
+//         // log this error if 'new' operator is not called
+//         throw Error("You must use the 'new' operator to call the constructor")
         
+//     }
+//     this.tittle = tittle;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = 'not read yet';
+//     this.info = () => `${this.tittle} by ${this.author}, ${this.pages} pages, ${this.read}`;
+// }
+
+
+// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295);
+
+
+// function Player(name, marker) {
+//   if (!new.target) {
+//     throw Error("You must use the 'new' operator to call the constructor");
+//   }
+//   this.name = name;
+//   this.marker = marker;
+//   this.sayName = function() {
+//     console.log(this.name)
+//   };
+// }
+
+// const player1 = new Player('steve', 'X');
+// const player2 = new Player('also steve', 'O');
+
+function Person(name) {
+    if (!new.target){
+        throw Error (`Use "New"`);
     }
-    this.tittle = tittle;
-    this.author = author;
-    this.pages = pages;
-    this.read = 'not read yet';
-    this.info = () => `${this.tittle} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  this.name = name;
 }
 
-
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295);
-
+Person.prototype.sayName = function() {
+  console.log(`Hello, I'm ${this.name}!`);
+};
 
 function Player(name, marker) {
-  if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
-  }
   this.name = name;
   this.marker = marker;
-  this.sayName = function() {
-    console.log(this.name)
-  };
 }
 
-const player1 = new Player('steve', 'X');
-const player2 = new Player('also steve', 'O');
+Player.prototype.getMarker = function() {
+  console.log(`My marker is '${this.marker}'`);
+};
+
+Object.getPrototypeOf(Player.prototype);
+
+
+function sayThis (param) {
+    return console.log(param);
+}
