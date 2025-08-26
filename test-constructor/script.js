9c28d46406f06edfa413e6cadc1e39011e994d18
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-08-26 23:22:31 +0800

    commit this changes

--- test-constructor/script.js
+++ test-constructor/script.js
@@ -1,18 +1,30 @@
-// function Book (tittle, author, pages) {
-//     if (!new.target){
-//         // log this error if 'new' operator is not called
-//         throw Error("You must use the 'new' operator to call the constructor")
+
+const myLibrary = [];
+
+function Book (tittle, author, pages) {
+    if (!new.target){
+        // log this error if 'new' operator is not called
+        throw Error("You must use the 'new' operator to call the constructor")
         
-//     }
-//     this.tittle = tittle;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = 'not read yet';
-//     this.info = () => `${this.tittle} by ${this.author}, ${this.pages} pages, ${this.read}`;
-// }
+    }
+    this.tittle = tittle;
+    this.author = author;
+    this.pages = pages;
+    this.read = 'not read yet';
+    this.info = () => `${this.tittle} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    this.id = crypto.randomUUID();
+}
+
+function addToLibrary (book) {
+    myLibrary.push(book);
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295);
 
 
-// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295);
+
+
+
 
 
 // function Player(name, marker) {
@@ -29,29 +41,29 @@
 // const player1 = new Player('steve', 'X');
 // const player2 = new Player('also steve', 'O');
 
-function Person(name) {
-    if (!new.target){
-        throw Error (`Use "New"`);
-    }
-  this.name = name;
-}
+// function Person(name) {
+//     if (!new.target){
+//         throw Error (`Use "New"`);
+//     }
+//   this.name = name;
+// }
 
-Person.prototype.sayName = function() {
-  console.log(`Hello, I'm ${this.name}!`);
-};
+// Person.prototype.sayName = function() {
+//   console.log(`Hello, I'm ${this.name}!`);
+// };
 
-function Player(name, marker) {
-  this.name = name;
-  this.marker = marker;
-}
+// function Player(name, marker) {
+//   this.name = name;
+//   this.marker = marker;
+// }
 
-Player.prototype.getMarker = function() {
-  console.log(`My marker is '${this.marker}'`);
-};
+// Player.prototype.getMarker = function() {
+//   console.log(`My marker is '${this.marker}'`);
+// };
 
-Object.getPrototypeOf(Player.prototype);
+// Object.getPrototypeOf(Player.prototype);
 
 
-function sayThis (param) {
-    return console.log(param);
-}
+// function sayThis (param) {
+//     return console.log(param);
+// }
